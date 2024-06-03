058169cb5c9271ed4e7fc14fc59db715a9e90c58
Legaseas <rachvilly@icloud.com>
2024-06-03 15:14:28 +0100

    fix to the camera and home screen

--- components/Camera/CustomCamera.tsx
+++ components/Camera/CustomCamera.tsx
@@ -1,84 +1,114 @@
 // CameraComponent.js
-import React, { useState, useRef, useEffect } from "react";
-import { View, StyleSheet, useWindowDimensions, Animated, Platform } from "react-native";
+import React, { useState, useRef, useEffect, ReactNode } from "react";
+import { View, StyleSheet, useWindowDimensions, Animated, Platform, Text } from "react-native";
 import { Camera, CameraView, useCameraPermissions } from "expo-camera";
 import * as ImageManipulator from "expo-image-manipulator";
 import { Colors } from "../Colors";
-import { MediumText, RegularText } from "../styles/styledComponents";
-import { Button } from "../Button/Button";
 import { BlurView } from "expo-blur";
 import Webcam, { WebcamProps } from "react-webcam";
 import * as ml5 from "ml5";
+import axios from "axios";
 
 type CameraT = {
   onPictureTaken?: (photo) => void;
+  children: ReactNode
   isVisible: boolean;
 };
 
+// TODO: Add these properties to the constant directory
 const allowedObjects = ['book', 'paper', 'person', 'tv', 'sheet']
+
 const dimensions = {
-  width: 800,
-  height: 500
+  cameraWidth: 500,
+  CameraHeight: 1000
 }
+
+// TEMP:
+const { cameraWidth, CameraHeight } = dimensions
 
 type VideoConstraints = { facingMode?: string; deviceId?: string };
 
-const CustomCamera = ({ onPictureTaken, isVisible }: CameraT) => {
-  const [hasPermission, setHasPermission] = useState(null);
+const CustomCamera = ({ onPictureTaken, isVisible, children }: CameraT) => {
+  // DECLARE ALL 
   const [permission, requestPermission] = useCameraPermissions();
+  const [hasPermission, setHasPermission] = useState(null);
   const [devices, setDevices] = React.useState([]);
-
-const [videoConstraints, setVideoConstraints] = useState<VideoConstraints>({
-  facingMode: "environment",
-});
-
-useEffect(() => {
-  const getBackCamera = async () => {
-    try {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = devices.filter(
-        (device) => device.kind === "videoinput"
-      );
-
-      // Check if facingMode is supported
-      const supportsFacingMode = videoDevices.some((device) =>
-        device.label.toLowerCase().includes("environment")
-      );
-
-      if (!supportsFacingMode) {
-        // Find a device with 'back' or 'rear' label as fallback
-        const backCamera = videoDevices.find(
-          (device) =>
-            device.label.toLowerCase().includes("back") ||
-            device.label.toLowerCase().includes("rear")
-        );
-
-        if (backCamera) {
-          setVideoConstraints({ deviceId: backCamera.deviceId });
-        } else {
-          console.error("Back camera not found");
-        }
-      }
-    } catch (error) {
-      console.error("Error enumerating devices: ", error);
-    }
-  };
-
-  getBackCamera();
-}, []);
-
 
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const [isUnfocused, setIsUnfocused] = useState(false)
-  const [capturedImage, setCapturedImage] = useState();
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [restartCamera, setRestartCamera] = useState(false)
 
   const cameraRef = useRef(null);
   const [model, setModel] = useState(null);
   const [predictions, setPredictions] = useState([]);
   const { fontScale, height, width } = useWindowDimensions();
-
   const opacity = useRef(new Animated.Value(isVisible ? 1 : 0)).current;
+
+  const [videoConstraints, setVideoConstraints] = useState<VideoConstraints>({
+    facingMode: "environment",
+  });
+
+  // Call the recognize end point
+  const handleImageUpload = async (imageBlob) => {
+    const data = new FormData();
+    data.append("image", imageBlob, 'lens_image.png')
+    // const response = await axios.post("http://localhost:9000/api/v1/payment/transfer/send-funds/scan-image", data, {
+    //   headers: {
+    //     "Content-Type": "multipart/form-data",
+    //   },
+    // });
+    console.log(imageBlob)
+    // if (response.success) {
+    //   // navigate("/dashboard/send-funds", { state: { data: response.data }, replace: true });
+    // }
+  };
+
+
+  const handleCapturedImage = (blob: Blob) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = (file) => {
+      setCapturedImage(file.target.result);
+    };
+    // Sending the blob directly as an image file
+    handleImageUpload(blob);
+  };
+
+  useEffect(() => {
+    const getBackCamera = async () => {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const videoDevices = devices.filter(
+          (device) => device.kind === "videoinput"
+        );
+
+        // Check if facingMode is supported
+        const supportsFacingMode = videoDevices.some((device) =>
+          device.label.toLowerCase().includes("environment")
+        );
+
+        if (!supportsFacingMode) {
+          // Find a device with 'back' or 'rear' label as fallback
+          const backCamera = videoDevices.find(
+            (device) =>
+              device.label.toLowerCase().includes("back") ||
+              device.label.toLowerCase().includes("rear")
+          );
+
+          if (backCamera) {
+            setVideoConstraints({ deviceId: backCamera.deviceId });
+          } else {
+            console.error("Back camera not found");
+          }
+        }
+      } catch (error) {
+        console.error("Error enumerating devices: ", error);
+      }
+    };
+
+    getBackCamera();
+  }, []);
 
   useEffect(() => {
     Animated.timing(opacity, {
@@ -98,12 +128,12 @@ useEffect(() => {
   useEffect(() => {
     let detectionInterval;
 
-    const modelLoaded = () => {
-      webcamRef.current.video.width = width;
-      webcamRef.current.video.height = height;
-      canvasRef.current.width = width;
-      canvasRef.current.height = height;
 
+    const modelLoaded = () => {
+      webcamRef.current.video.width = cameraWidth;
+      webcamRef.current.video.height = CameraHeight;
+      canvasRef.current.width = cameraWidth;
+      canvasRef.current.height = CameraHeight;
 
       detectionInterval = setInterval(() => {
         detect();
@@ -120,27 +150,22 @@ useEffect(() => {
 
       try {
         objectDetector.detect(webcamRef.current.video, (err, results) => {
-          const ctx = canvasRef.current.getContext('2d');
-          ctx.clearRect(0, 0, width, height);
-          canvasRef.current
           if (results && results.length) {
             console.log(results)
-            if (results.length > 1) {
-              setIsUnfocused(true)
-            }
-            else {
-              setIsUnfocused(false)
-              for (let all of results) {
-                if (allowedObjects.includes(all.label)) {
-                  ctx.drawImage(webcamRef.current.video, 0, 0, width, height);
-                  // canvasRef.current.toBlob(handleCapturedImage);
-                }
+            for (let all of results) {
+              if (allowedObjects.includes(all.label)) {
+                const ctx = canvasRef.current.getContext('2d');
+                // ctx.clearRect(0, 0, width, height);
+                // ctx.drawImage(webcamRef.current.video, 0, 0, width, height);
+                canvasRef.current.toBlob(handleCapturedImage);
+                clearInterval(detectionInterval)
               }
             }
           }
         });
       } catch (e) {
         console.log(e);
+        clearInterval(detectionInterval)
       }
     };
 
@@ -150,7 +175,7 @@ useEffect(() => {
       }
     }
 
-  }, [width, height]);
+  }, [width, height, restartCamera]);
 
   useEffect(() => {
     (async () => {
@@ -163,8 +188,8 @@ useEffect(() => {
   return (
     <Animated.View
       style={{
-        position: "absolute",
-        height: height / 1.7,
+        // position: "absolute",
+        height: height / 2.4,
         width: "100%",
         top: 0,
         borderBottomLeftRadius: 30,
@@ -176,54 +201,32 @@ useEffect(() => {
         {Platform.OS === "web" ? (
           <>
             <Webcam
-              style={styles.camera}
               ref={webcamRef}
-              className="webcam"
+              style={styles.camera}
               videoConstraints={videoConstraints}
             />
-              <canvas ref={canvasRef} className="canvas" />
+            <canvas style={styles.camera} ref={canvasRef} />
           </>
         ) : (
           <CameraView ref={cameraRef} style={StyleSheet.absoluteFillObject} />
         )}
+
+        <BlurView
+          style={styles.blurView}
+          tint="dark"
+          intensity={30}
+          experimentalBlurMethod="dimezisBlurView"
+        />
       </View>
 
-      <BlurView
-        style={{
-          position: "absolute",
-          height: "100%",
-          width: "100%",
-          top: 0,
-          borderBottomLeftRadius: 10,
-          borderBottomRightRadius: 10,
-          overflow: "hidden",
-        }}
-        tint="dark"
-        intensity={30}
-        experimentalBlurMethod="dimezisBlurView"
-      />
+      <View style={{ position: 'absolute', left: 0, right: 0 }}>
+        {children}
+      </View>
     </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
-  centeredView: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-    backgroundColor: Colors.white,
-    gap: 10,
-  },
-
-  container: {
-    position: "absolute",
-    height: "40%",
-    width: "100%",
-    top: 0,
-    overflow: "hidden",
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
   cameraWrapper: {
     flex: 1,
     overflow: "hidden",
@@ -240,6 +243,7 @@ const styles = StyleSheet.create({
     top: 0,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
+    overflow: "hidden",
   },
 });
 
